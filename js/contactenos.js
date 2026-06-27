648bbdd5b03469acd6bd22ce99b0e8bc25d41883
Juan Vivas <j.vivas20021@gmail.com>
2026-06-27 09:47:45 -0500

    V1

--- js/contactenos.js
+++ js/contactenos.js
@@ -1,1 +1,24 @@
-let hola;
+/* const formulario = document.getElementById('contactenos');
+
+formulario.addEventListener("submit", function (evento) {
+    evento.preventDefault();
+
+    const nombre = document.getElementById('nombreCompleto').value;
+    const email = document.getElementById('agregarCorreo').value;
+    const telefono = document.getElementById('numeroTelefono').value;
+    const indicativo = document.getElementById('numIndicativo').value;
+    const mensaje = document.getElementById('escribaMensaje').value;
+
+    const usuario = {
+        nombre,
+        email,
+        indicativo,
+        telefono,
+        mensaje
+    };
+
+    localStorage.setItem("formulario", JSON.stringify(usuario));
+    console.log("Datos guardados:", usuario);
+    alert("Tú comentario fue enviado correctamente");
+    formulario.reset();
+}); */
